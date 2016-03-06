59a495eae1ad49126915b5ad918a0defc08080e5
lcarter347 <lcarter3@mail.umw.edu>
2016-03-06 17:23:13 +0000

    Implement search, narrow results by subject

--- BuyMyBooks/buymybooks.sql
+++ BuyMyBooks/buymybooks.sql
@@ -19,10 +19,9 @@ CREATE TABLE users (
 
 DROP TABLE IF EXISTS listedbooks;
 CREATE TABLE listedbooks (
-  id SERIAL PRIMARY KEY,
+  bookid SERIAL PRIMARY KEY,
   isbn TEXT NOT NULL,
   title TEXT NOT NULL,
-  author TEXT NOT NULL,
   price MONEY NOT NULL,
   subject TEXT NOT NULL,
   description TEXT,
@@ -31,16 +30,28 @@ CREATE TABLE listedbooks (
   sold BOOLEAN NOT NULL DEFAULT FALSE
 );
 
+DROP TABLE IF EXISTS authors;
+CREATE TABLE authors (
+  authorid SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS authortobook;
+CREATE TABLE authortobook (
+  authorid INT NOT NULL REFERENCES authors(authorid),
+  bookid INT NOT NULL REFERENCES listedbooks(bookid)
+);
+
 DROP TABLE IF EXISTS soldbooks;
 CREATE TABLE soldbooks (
-  bookid INT NOT NULL REFERENCES listedbooks(id),
+  bookid INT NOT NULL REFERENCES listedbooks(bookid),
   userid TEXT NOT NULL REFERENCES users(email),
   purchasedate DATE NOT NULL
 );
 
 DROP TABLE IF EXISTS cart;
 CREATE TABLE cart(
-  bookid INT NOT NULL REFERENCES listedbooks(id),
+  bookid INT NOT NULL REFERENCES listedbooks(bookid),
   userid TEXT NOT NULL REFERENCES users(email)
 );
   
@@ -48,14 +59,40 @@ CREATE TABLE cart(
 INSERT INTO users VALUES ('lcarter@mail.com', 'Lisa', 'Carter', 'University of Mary Washington', crypt('password', gen_salt('bf')));
 INSERT INTO users VALUES ('sholt@mail.com', 'Steve', 'Holt', 'University of Southern California', crypt('password', gen_salt('bf')));
 
-INSERT INTO listedbooks (isbn, title, author, price, subject, description, pictureurl, userid ) VALUES ('0596526849', 'Head First SQL', 'Lynn Beighley', '20', 'Computer Science', '', 'https://books.google.com/books/content?id=ZO6MF9Ja1zoC&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE71EpWayqKSolAUG6tzzxQICvYhHdRjbxi8AaAhNb642kn17Tr4wOSbV1S68BIxLaPokCfKuZ_KVrxz9lSEVOBJxk0v8vuw5GUWdMegnSHmFKauZ3ki9lES5s7RrgHOA95lUGUxz', 'lcarter@mail.com');
-INSERT INTO listedbooks (isbn, title, author, price, subject, description, pictureurl, userid ) VALUES ('0132856204', 'Computer Networking', 'James Kurose', '50', 'Computer Science', 'Message', 'http://ecx.images-amazon.com/images/I/51xfeAQU6dL._SX414_BO1,204,203,200_.jpg', 'lcarter@mail.com');
+INSERT INTO authors (name) VALUES ('Marty Knorre');
+INSERT INTO authors (name) VALUES ('Thalia Dorwick');
+INSERT INTO authors (name) VALUES ('Ana Maria Perez-Girones');
+INSERT INTO authors (name) VALUES ('William R. Glass');
+INSERT INTO authors (name) VALUES ('Hildebrando Villarreal');
+INSERT INTO listedbooks (isbn, title, price, subject, description, pictureurl, userid ) VALUES ('0073534420', 'Puntos de Partida', '10', 'Foreign Language', '', 'http://ecx.images-amazon.com/images/I/51WykD8deWL._SX398_BO1,204,203,200_.jpg', 'lcarter@mail.com');
+INSERT INTO authortobook (authorid, bookid) VALUES (1, 1);
+INSERT INTO authortobook (authorid, bookid) VALUES (2, 1);
+INSERT INTO authortobook (authorid, bookid) VALUES (3, 1);
+INSERT INTO authortobook (authorid, bookid) VALUES (4, 1);
+INSERT INTO authortobook (authorid, bookid) VALUES (5, 1);
+
+INSERT INTO authors (name) VALUES ('James F. Kurose');
+INSERT INTO authors (name) VALUES ('Keith W. Ross');
+INSERT INTO listedbooks (isbn, title, price, subject, description, pictureurl, userid ) VALUES ('0132856204', 'Computer Networking', '40', 'Computer Science', '', 'http://ecx.images-amazon.com/images/I/51xfeAQU6dL._SX414_BO1,204,203,200_.jpg', 'lcarter@mail.com');
+INSERT INTO authortobook (authorid, bookid) VALUES (6, 2);
+INSERT INTO authortobook (authorid, bookid) VALUES (7, 2);
+
+INSERT INTO authors (name) VALUES ('Lynn Beighley');
+INSERT INTO listedbooks (isbn, title, price, subject, description, pictureurl, userid ) VALUES ('0596526849', 'Head First SQL', '10', 'Computer Science', '', 'http://ecx.images-amazon.com/images/I/51F8NgTHQOL._SX431_BO1,204,203,200_.jpg', 'lcarter@mail.com');
+INSERT INTO authortobook (authorid, bookid) VALUES (8, 3);
+
+INSERT INTO authors (name) VALUES ('Luke Eric Lassiter');
+INSERT INTO listedbooks (isbn, title, price, subject, description, pictureurl, userid ) VALUES ('0759111537', 'Invitation to Anthropology', '10', 'Anthropology', '', 'http://ecx.images-amazon.com/images/I/41wPfysCLDL._SX331_BO1,204,203,200_.jpg', 'lcarter@mail.com');
+INSERT INTO authortobook (authorid, bookid) VALUES (9, 4);
 
 DROP ROLE IF EXISTS bookuser;
 CREATE ROLE bookuser LOGIN PASSWORD 'Lv+;92>&';
 GRANT SELECT, INSERT on users TO bookuser;
 GRANT SELECT, INSERT, UPDATE on listedbooks TO bookuser;
-GRANT USAGE on listedbooks_id_seq TO bookuser;
+GRANT SELECT, INSERT on authors TO bookuser;
+GRANT SELECT, INSERT on authortobook TO bookuser;
+GRANT USAGE on listedbooks_bookid_seq TO bookuser;
+GRANT USAGE on authors_authorid_seq TO bookuser;
 GRANT SELECT, INSERT on soldbooks TO bookuser;
 GRANT SELECT, INSERT, UPDATE, DELETE on cart TO bookuser;
 
--- BuyMyBooks/server.py
+++ BuyMyBooks/server.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+import os
 import psycopg2
 import psycopg2.extras
-import os
+from flask import Flask, session, render_template, request, redirect, url_for, make_response
+from flask.ext.socketio import SocketIO, emit
+import unicodedata
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
+socketio = SocketIO(app)
 
 def connectToDB():
     connectionString = 'dbname=buymybooks user=bookuser password=Lv+;92>& host=localhost'
@@ -15,6 +18,52 @@ def connectToDB():
     except:
         print "Cannot connect to database"
 
+
+@socketio.on('connect', namespace='/search')
+def connection():
+    print('connected')
+    
+@socketio.on('search', namespace='/search')    
+def search(txt, subjectfilter):
+    description = "No description available"
+    conn = connectToDB()
+    cur = conn.cursor()
+    txt = txt.lower()
+    txt = remove_accents(txt)
+    print txt
+    txt = '%' + txt + '%'
+    if (subjectfilter=="all"):
+        query = cur.mogrify("""SELECT lb.isbn, STRING_AGG(a.name, ', '), lb.title, lb.price, lb.subject, \
+            lb.description, lb.pictureurl FROM listedbooks as lb INNER JOIN authortobook as atb ON lb.bookid=atb.bookid \
+            INNER JOIN authors as a ON atb.authorid=a.authorid WHERE LOWER(lb.title) LIKE %s OR LOWER(a.name) LIKE %s \
+            OR lb.isbn Like %s AND lb.sold=False GROUP BY lb.bookid;""", (txt, txt, txt))
+    else:
+        q = "SELECT lb.isbn, STRING_AGG(a.name, ', '), lb.title, lb.price, lb.subject, lb.description, lb.pictureurl \
+            FROM listedbooks as lb INNER JOIN authortobook as atb ON lb.bookid=atb.bookid INNER JOIN authors as a ON \
+            atb.authorid=a.authorid WHERE (lb.subject IN (" + subjectfilter + ") AND lb.sold=False) AND (LOWER(lb.title) \
+            LIKE %s OR LOWER(a.name) LIKE %s OR lb.isbn LIKE %s) GROUP BY lb.bookid;"
+        query = cur.mogrify(q, (txt, txt, txt))
+    
+    print query
+    cur.execute(query)  
+    results = cur.fetchall()
+    print results
+    if results != []:
+        for result in results:
+            if result[5] != "":
+                description = result[5]
+            tmp = {'isbn':result[0], 'title':result[2], 'author':result[1],
+            'price':result[3], 'subject':result[4], 'description':description, 
+            'pictureurl':result[6]}
+            emit('search', tmp)
+    else:
+        emit('noresults')
+        print "No results"
+    
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])    
+    
 @app.route('/', methods=['GET', 'POST'])
 def mainIndex():
     loggedIn = False
@@ -105,17 +154,48 @@ def sell():
         if request.method == 'POST':
             conn = connectToDB()
             cur = conn.cursor()
+            bookid = 0
+            authorids = []
+            authors = []
             try:
                 if request.form.get('description'):
-                    description = request.form['description']
+                    description = remove_accents(request.form['description'])
                 if request.form.get('url'):
                     pictureurl = request.form['url']
-                q = "INSERT INTO listedbooks (isbn, title, author, price, subject, description, pictureurl, userid ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
-                query = cur.mogrify(q, (request.form['isbn'], request.form['title'], 
-                    request.form['author'], request.form['price'], request.form['subject'], 
-                    description, pictureurl, currentUser))
+                authors = [x.strip() for x in request.form['author'].split(',')]
+                print(authors)
+                for a in authors:
+                    a = remove_accents(a)
+                    query = cur.mogrify("INSERT INTO authors (name) VALUES (%s);", (a,))
+                    print cur.mogrify(query)
+                    cur.execute(query)
+                    query = cur.mogrify("SELECT authorid FROM authors ORDER BY authorid DESC LIMIT 1;")
+                    print query
+                    cur.execute(query)
+                    results = cur.fetchall()
+                    print results
+                    if results != []:
+                        for result in results:
+                            authorids.append(result[0]) 
+                    print(authorids)
+                title = remove_accents(request.form['title'])
+                q = "INSERT INTO listedbooks (isbn, title, price, subject, description, pictureurl, userid ) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+                query = cur.mogrify(q, (request.form['isbn'], title, request.form['price'], 
+                    request.form['subject'], description, pictureurl, currentUser))
                 print cur.mogrify(query)
                 cur.execute(query)
+                query = cur.mogrify("SELECT bookid FROM listedbooks ORDER BY bookid DESC LIMIT 1;")
+                cur.execute(query)
+                results = cur.fetchall()
+                print results
+                if results != []:
+                    for result in results:
+                        bookid = result[0]
+                for id in authorids:
+                    q = "INSERT INTO authortobook (authorid, bookid) VALUES (%s, %s);"
+                    query = cur.mogrify(q, (id, bookid))
+                    print cur.mogrify(query)
+                    cur.execute(query)
                 listingSuccess = True
             except:
                 print("Error inserting book listing")
@@ -159,6 +239,8 @@ def account():
         if results != []:
             for result in results:
                 accountInfo = {"email":result[0], 'firstname':result[1], 'lastname':result[2], 'school':result[3]}
+        """q= "SELECT isbn, title, author, price, subject, description FROM listedbooks  WHERE email = %s;"
+        query = cur.mogrify(q, (currentUser,))"""
     return render_template('account.html', loggedIn=loggedIn, accountInfo=accountInfo)
     
 @app.route('/single', methods=['GET', 'POST'])
@@ -178,5 +260,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.debug=True
-    app.run(host='0.0.0.0', port=8080)
+    socketio.run(app, host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
